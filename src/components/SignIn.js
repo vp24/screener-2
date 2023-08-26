e7c7aae66109a4a66fda0ffda35811741e9ec78e
VP <vishay8@gmail.com>
2023-08-26 10:43:25 -0500

    fixing signin

--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -1,14 +1,16 @@
 import axios from 'axios';
-import { useState } from 'react';
+import { useState, useEffect } from 'react'; // Add useEffect import
 import { useNavigate } from 'react-router-dom';
 
 function SignIn({ onSignIn, isAuthenticated }) {
   const navigate = useNavigate();
 
-  // Redirect if already authenticated
-  if (isAuthenticated) {
-    navigate('/');  // Use navigate instead of history.push
-  }
+  useEffect(() => {
+    // Redirect if already authenticated
+    if (isAuthenticated) {
+      navigate('/');  // Use navigate instead of history.push
+    }
+  }, [isAuthenticated, navigate]);  // Dependency array to re-run the effect if isAuthenticated or navigate changes
 
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
@@ -32,7 +34,7 @@ function SignIn({ onSignIn, isAuthenticated }) {
   return (
     <div className="signin-container container">
       <h2>Sign In</h2>
-      <form className="signin-form" onSubmit={handleSubmit}> {/* Added onSubmit event here */}
+      <form className="signin-form" onSubmit={handleSubmit}>
         <input className="signin-input" type="text" value={username} onChange={(e) => setUsername(e.target.value)} placeholder="Username" required />
         <input className="signin-input" type="password" value={password} onChange={(e) => setPassword(e.target.value)} placeholder="Password" required />
         <button type="submit" className="signin-btn">Sign In</button>
