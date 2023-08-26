e24e59159ceeb9df5054aa77f949780727637335
VP <vishay8@gmail.com>
2023-08-26 11:40:50 -0500

    the last 3 years from the table

--- src/components/Financials.js
+++ src/components/Financials.js
@@ -11,45 +11,59 @@ const Financials = ({ data, scrapedLink }) => {
   const iseTableA = findTable("iseTableA");
   const bsTable = findTable("bsTable");
 
-  const allYears = valuationTable[0] ? valuationTable[0].slice(1) : [];
-  const latestThreeYears = allYears.slice(-3);
+  const getYearFromTable = (table, index) => table[0]?.[index];
+
+  const lastYearIndex = valuationTable[0].length - 1;
+  const years = [lastYearIndex - 2, lastYearIndex - 1, lastYearIndex].map(i => getYearFromTable(valuationTable, i));
+
+  let mkCapLabel = valuationTable[1]?.[0];
+  if (mkCapLabel === "Capitalization") {
+    mkCapLabel = "Mkt Cap";
+  }
+  let peRatioLabel = valuationTable[3]?.[0];
+  let yieldLabel = valuationTable[4]?.[0];
+  let netSalesLabel = iseTableA[1]?.[0];
+  let netIncomeLabel = iseTableA[6]?.[0];
+  let netDebtLabel = bsTable[1]?.[0];
+  let netCashLabel = bsTable[2]?.[0];
 
   const convertToSuperscript = (label) => {
-    const superscriptMap = {
-      "0": "⁰",
-      "1": "¹",
-      "2": "²",
-      "3": "³",
-      "4": "⁴",
-      "5": "⁵",
-      "6": "⁶",
-      "7": "⁷",
-      "8": "⁸",
-      "9": "⁹",
-    };
-    return label.replace(/([a-zA-Z]+)\s?(\d+)/g, (_, text, number) => text + (superscriptMap[number] || ""));
+    return label.replace(/([a-zA-Z]+)\s?(\d+)/g, (_, text, number) => {
+      const superscriptMap = {
+        "0": "⁰",
+        "1": "¹",
+        "2": "²",
+        "3": "³",
+        "4": "⁴",
+        "5": "⁵",
+        "6": "⁶",
+        "7": "⁷",
+        "8": "⁸",
+        "9": "⁹",
+      };
+      return text + (superscriptMap[number] || "");
+    });
   };
 
   const capitalize = (label) => {
-    return label.split(' ').map(s => s.charAt(0).toUpperCase() + s.substring(1)).join(' ');
+    return label
+      .split(' ')
+      .map((s) => s.charAt(0).toUpperCase() + s.substring(1))
+      .join(' ');
   };
 
-  const formatLabel = (label) => capitalize(convertToSuperscript(label));
-
-  const labels = {
-    mkCap: formatLabel(valuationTable[1]?.[0] === "Capitalization" ? "Mkt Cap" : valuationTable[1]?.[0]),
-    peRatio: formatLabel(valuationTable[3]?.[0]),
-    yield: formatLabel(valuationTable[4]?.[0]),
-    netSales: formatLabel(iseTableA[1]?.[0]),
-    netIncome: formatLabel(iseTableA[6]?.[0]),
-    netDebt: formatLabel(bsTable[1]?.[0]),
-    netCash: formatLabel(bsTable[2]?.[0]),
-  };
+  mkCapLabel = capitalize(convertToSuperscript(mkCapLabel));
+  peRatioLabel = capitalize(convertToSuperscript(peRatioLabel));
+  yieldLabel = capitalize(convertToSuperscript(yieldLabel));
+  netSalesLabel = capitalize(convertToSuperscript(netSalesLabel));
+  netIncomeLabel = capitalize(convertToSuperscript(netIncomeLabel));
+  netDebtLabel = capitalize(convertToSuperscript(netDebtLabel));
+  netCashLabel = capitalize(convertToSuperscript(netCashLabel));
 
   return (
     <div>
       <div className="mkt-cap">
-        <h3><strong>{labels.mkCap}: {valuationTable[1]?.[6]}</strong></h3>
+        <h3><strong>{mkCapLabel}: {valuationTable[1]?.[6]}</strong></h3>
       </div>
       <div className="center">
         <Pr10TextsDisplay pr10Texts={data.map(({ pr10Text }) => pr10Text)} />
@@ -57,15 +71,15 @@ const Financials = ({ data, scrapedLink }) => {
       <div className="financials">
         <h2>Financials</h2>
         <div className="financials-grid">
-          {latestThreeYears.map((year, i) => (
+          {years.map((year, i) => (
             <div key={i}>
               <h3>{year}</h3>
-              <p><strong>{labels.netSales}</strong>: {iseTableA[1]?.[allYears.length - 3 + i]}</p>
-              <p><strong>{labels.netIncome}</strong>: {iseTableA[6]?.[allYears.length - 3 + i]}</p>
-              <p><strong>{labels.netCash}</strong>: {bsTable[2]?.[allYears.length - 3 + i]}</p>
-              <p><strong>{labels.netDebt}</strong>: {bsTable[1]?.[allYears.length - 3 + i]}</p>
-              <p><strong>{labels.peRatio}</strong>: {valuationTable[3]?.[allYears.length - 3 + i]}</p>
-              <p><strong>{labels.yield}</strong>: {valuationTable[4]?.[allYears.length - 3 + i]}</p>
+              <p><strong>{netSalesLabel}</strong>: {iseTableA[1]?.[year]}</p>
+              <p><strong>{netIncomeLabel}</strong>: {iseTableA[6]?.[year]}</p>
+              <p><strong>{netCashLabel}</strong>: {bsTable[2]?.[year]}</p>
+              <p><strong>{netDebtLabel}</strong>: {bsTable[1]?.[year]}</p>
+              <p><strong>{peRatioLabel}</strong>: {valuationTable[3]?.[year]}</p>
+              <p><strong>{yieldLabel}</strong>: {valuationTable[4]?.[year]}</p>
             </div>
           ))}
         </div>
