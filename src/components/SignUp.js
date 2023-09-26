e920ae4d6ffe2c3ee45710240ab2d2e2dc783b43
VP <vishay8@gmail.com>
2023-09-26 13:03:42 -0500

    better signup ux

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -1,38 +1,41 @@
 import axios from 'axios';
 import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+//import './SignUp.css'; 
 
 function SignUp() {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
-  const navigate = useNavigate();  // Added for navigation
+  const [error, setError] = useState('');  // State to store the error message
+  const navigate = useNavigate();
 
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      // const response = await axios.post('https://screener-api.onrender.com/signup', {
-        const response = await axios.post('https://tiny-jade-ostrich-tux.cyclic.cloud/signup', {
-
+      const response = await axios.post('https://tiny-jade-ostrich-tux.cyclic.cloud/signup', {
         username,
         password,
       });
+      
       if (response.data && response.data.message) {
         console.log(`User ${username} signed up with message: ${response.data.message}`);
-        alert('Successfully signed up! Redirecting to Sign In page...');  // Feedback for user
+        alert('Successfully signed up! Redirecting to Sign In page...');
+        setError('');  // Clear the error state on successful sign up
         navigate('/signin');  // Redirect to Sign In page
       }
     } catch (error) {
-        console.error("Error signing up", error);
-        alert('Sign up failed. Please try again.');  // Feedback for user
+      const errorMessage = error.response?.data?.error || 'Sign up failed. Please try again.';
+      setError(errorMessage);  // Set the error state with the specific error message
     }
   };
 
   return (
     <div className="signup-container container">
       <h2>Sign Up</h2>
-      <form className="signup-form" onSubmit={handleSubmit}> {/* Added onSubmit event here */}
+      <form className="signup-form" onSubmit={handleSubmit}>
         <input className="signup-input" type="text" value={username} onChange={(e) => setUsername(e.target.value)} placeholder="Username" required />
         <input className="signup-input" type="password" value={password} onChange={(e) => setPassword(e.target.value)} placeholder="Password" required />
+        {error && <div className="error-message">{error}</div>} {/* Display error messages */}
         <button type="submit" className="signup-btn">Sign Up</button>
       </form>
     </div>
