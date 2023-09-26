569bd5aedd9b5235070c81b845bbb9dee1520fac
VP <vishay8@gmail.com>
2023-09-26 13:26:29 -0500

    better signin and signup ux

--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -1,34 +1,33 @@
 import axios from 'axios';
-import { useState, useEffect } from 'react'; // Add useEffect import
+import { useState, useEffect } from 'react'; 
 import { useNavigate } from 'react-router-dom';
 
 function SignIn({ onSignIn, isAuthenticated }) {
   const navigate = useNavigate();
-
-  useEffect(() => {
-    // Redirect if already authenticated
-    if (isAuthenticated) {
-      navigate('/');  // Use navigate instead of history.push
-    }
-  }, [isAuthenticated, navigate]);  // Dependency array to re-run the effect if isAuthenticated or navigate changes
-
+  
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
+  const [errorMsg, setErrorMsg] = useState(''); // State for error message
+
+  useEffect(() => {
+    if (isAuthenticated) {
+      navigate('/');
+    }
+  }, [isAuthenticated, navigate]);
 
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      // const response = await axios.post('https://screener-api.onrender.com/signin', {
-        const response = await axios.post('https://tiny-jade-ostrich-tux.cyclic.cloud/signin', {
-
+      const response = await axios.post('https://tiny-jade-ostrich-tux.cyclic.cloud/signin', {
         username,
         password,
       });
       if (response.data && response.data.token) {
         onSignIn(username);
-        navigate('/');  // Redirect to the main page using navigate
+        navigate('/');
       }
     } catch (error) {
+      setErrorMsg(error.response?.data?.errorMessage || "Error signing in. Please try again."); // Set the error message based on server response or a general message
       console.error("Error signing in", error.response?.data || error.message);
     }
   };
@@ -36,6 +35,7 @@ function SignIn({ onSignIn, isAuthenticated }) {
   return (
     <div className="signin-container container">
       <h2>Sign In</h2>
+      {errorMsg && <p className="error-message">{errorMsg}</p>} {/* Display error message */}
       <form className="signin-form" onSubmit={handleSubmit}>
         <input className="signin-input" type="text" value={username} onChange={(e) => setUsername(e.target.value)} placeholder="Username" required />
         <input className="signin-input" type="password" value={password} onChange={(e) => setPassword(e.target.value)} placeholder="Password" required />
